f41963d83f78bedeb84f22fe0a5afa3266d8b581
Angat77 <angat.yogi@ou.edu>
2023-06-14 18:38:16 -0500

    graph component readded, passed name & symbol p2c

--- StockStrainer-UI/src/app/WatchList/watchlist-routing.module.ts
+++ StockStrainer-UI/src/app/WatchList/watchlist-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core'
 import { Routes } from '@angular/router'
 import { NativeScriptRouterModule } from '@nativescript/angular'
-// import {GraphComponent} from '../WatchList/Graph/graph.component'
+ import {GraphComponent} from '../WatchList/Graph/graph.component'
 
 import { WatchListComponent } from './watchlist.component'
 
 const routes: Routes = [{ path: 'default', component: WatchListComponent },
-// { path: 'graph', component: GraphComponent },
+ { path: 'graph', component: GraphComponent },
 ]
 
 @NgModule({
--- StockStrainer-UI/src/app/WatchList/watchlist.module.ts
+++ StockStrainer-UI/src/app/WatchList/watchlist.module.ts
@@ -1,12 +1,13 @@
 import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core'
 import { NativeScriptCommonModule } from '@nativescript/angular'
+import { GraphComponent } from './Graph/graph.component'
 
 import {WatchListRoutingModule} from './watchlist-routing.module'
 import { WatchListComponent } from './watchlist.component'
 
 @NgModule({
   imports: [NativeScriptCommonModule, WatchListRoutingModule],
-  declarations: [WatchListComponent],
+  declarations: [WatchListComponent,GraphComponent],
   schemas: [NO_ERRORS_SCHEMA],
 })
 export class WatchListModule {}
